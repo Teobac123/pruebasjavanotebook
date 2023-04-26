216b1fdae14faafbb77787d0b16f9df7c92ec501
Mateo Baccillere <mateobaccillere1@gmail.com>
2023-04-26 18:35:39 -0300

    agrege hola mundo

--- prueba.java
+++ prueba.java
@@ -3,7 +3,5 @@ public class prueba{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
     }
 }
