615cbd3a998e8596897a015e6904ba96077b736a
Mateo Baccillere <mateobaccillere1@gmail.com>
2023-04-26 15:11:15 -0300

    Modificando y creando archivos

--- prueba.java
+++ prueba.java
@@ -2,5 +2,8 @@
 public class prueba{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
 }
